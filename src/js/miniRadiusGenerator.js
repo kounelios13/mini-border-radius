96d2926eb93c10c499df60319f4f24ebb96c81c0
kounelios13 <pkounelios@gmail.com>
2016-01-29 23:03:40 +0200

    rename variables

--- src/js/miniRadiusGenerator.js
+++ src/js/miniRadiusGenerator.js
@@ -3,18 +3,21 @@ function Generator(app_container_id){
 	this.val=function(o){
 		return $(o).val();
 	}
-	this.generatorContainer="<div id='generatorContainer'>\n";
-	this.generatorContainer+="<input type='range' class='radius_slider' id='top_left_corner' min='0' max='200' value='0'>\n";
-	this.generatorContainer+="<input type='range' class='radius_slider' id='top_right_corner' min='0' max='200' value='0'>\n";
-	this.generatorContainer+="<div class='generatorOutput'></div>\n";
-	this.generatorContainer+="<input type='range' class='radius_slider' id='bottom_right_corner' min='0' max='200' value='0'>\n";
-	this.generatorContainer+="<input type='range' class='radius_slider' id='bottom_left_corner' min='0' max='200' value='0'></div>\n";
+	this.generator_markup="<div id='generatorContainer'>\n";
+	this.generator_markup+="<input type='range' class='radius_slider' id='top_left_corner' min='0' max='200' value='0'>\n";
+	this.generator_markup+="<input type='range' class='radius_slider' id='top_right_corner' min='0' max='200' value='0'>\n";
+	this.generator_markup+="<div class='generatorOutput'></div>\n";
+	this.generator_markup+="<input type='range' class='radius_slider' id='bottom_right_corner' min='0' max='200' value='0'>\n";
+	this.generator_markup+="<input type='range' class='radius_slider' id='bottom_left_corner' min='0' max='200' value='0'></div>\n";
+	this.generator_markup+="<div class='panel panel-primary'>\n<div class='panel-body text-center'>";
+	this.generator_markup+="border-radius: <span class='border_radius_code_output'></span>;\n</div></div>";
 	//the id of the container the generator will be added in
 	this.app_container_id=app_container_id;
-	$(this.app_container_id).append(this.generatorContainer);
+	$(this.app_container_id).append(this.generator_markup);
 	this.activateGenerator=function(){
 		var sliders=$(this.app_container_id+" .radius_slider");
 		this.code=this.val(sliders[0])+"px "+this.val(sliders[1])+"px "+this.val(sliders[2])+"px "+this.val(sliders[3])+"px";
+		$(this.app_container_id+" .border_radius_code_output").text(this.code);
 		$(this.app_container_id+" .generatorOutput").css("border-radius",this.code);
 	};//this will be used inside an event listener to activate the generator
 	this.getCode=function(){
