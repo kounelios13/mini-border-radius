8e84fd44d9b64f8f347596110d783236585f1ff4
kounelios13 <pkounelios@gmail.com>
2016-02-06 16:31:32 +0200

    updated demo page script

--- src/js/demo.js
+++ src/js/demo.js
@@ -4,8 +4,8 @@ var gens=[];
 $(document).ready(function(){
 	gen=new Generator("#sample","#demo_object_1");
 	default_options=gen.getOptions();
-	gen2=new Generator(["#sample2","12em","12em",300,'orange'],"#demo_object_2");
-	gen3=new Generator(["#sample3","16em","16em",400,'#cd24bf'],"#demo_object_3");
+	gen2=new Generator(["#sample2","12em","12em",200,'orange'],"#demo_object_2");
+	gen3=new Generator(["#sample3","16em","16em",300,'#fb24a2'],"#demo_object_3");
 	$("#bind_move").click(function(){
 		gen.activateGenerator();
 		gen2.activateGenerator();
