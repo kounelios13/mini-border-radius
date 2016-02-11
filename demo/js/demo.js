e92d4bff7bcd606315f86237767fda2f9c54512b
kounelios13 <pkounelios@gmail.com>
2016-02-11 10:22:31 +0200

    fixed demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -2,18 +2,18 @@ var generator;
 var activated = false;
 $(document).ready(function(){
 	$("#create_generator").on("click",function(){
-		generator=new Generator("#sample");
-		
+			generator=new Generator("#sample");
+			$(this).remove();
 	});
 	$("#activate_generator").on("click",function(){
 		if(!activated){
 			generator.activateGenerator();
 			activated=true;
 			alert("Generator activated.Please go and check if the generator is working.");
+			window.location.assign(window.location.pathname.split("#")[0]+"#sample");
+			
+			//window.location.assign(window.location.split("#")[0]+'#sample');
 		}
-		else{
-			alert("Generator has already been activated");
-		}
-		
+	
 	});
 });
